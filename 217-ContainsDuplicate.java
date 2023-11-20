72b6c6cc65196a83874b1a851ee3c308e00d0137
Tanujarora100 <108284546+Tanujarora100@users.noreply.github.com>
2023-11-21 00:45:19 +0530

    Create 217-ContainsDuplicate.java

---
+++ 217-ContainsDuplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+   public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++)
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        boolean result = false;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet())
+            if (entry.getValue() > 1)
+                result = true;
+
+        return result;
+    }
+}
